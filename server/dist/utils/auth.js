e5fc9e5e30dde58643c477ffe46f1b0f617436c1
MAGHC <windowsapple95@gmail.com>
2023-03-22 19:49:18 +0900

    util : auth 상수 생성

---
+++ server/dist/utils/auth.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AUTH_SUCCESS = exports.AUTH_ERRORS = void 0;
+exports.AUTH_ERRORS = Object.freeze({
+    ISEXIST: '이미 존재하는 아이디입니다',
+});
+exports.AUTH_SUCCESS = Object.freeze({
+    createID: ' 계정이 생성되었습니다',
+});
+//# sourceMappingURL=auth.js.map
---
+++ server/src/utils/auth.ts
@@ -0,0 +1,7 @@
+export const AUTH_ERRORS = Object.freeze({
+  ISEXIST: '이미 존재하는 아이디입니다',
+});
+
+export const AUTH_SUCCESS = Object.freeze({
+  createID: ' 계정이 생성되었습니다',
+});

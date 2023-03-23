68594c660c74f4e1f4369b214382cff82c062110
MAGHC <windowsapple95@gmail.com>
2023-03-23 20:59:19 +0900

    feat: useValidation 작성

---
+++ client/src/hooks/validation.tsx
@@ -0,0 +1,68 @@
+import { ChangeEvent, useState } from 'react';
+
+import { EMAIL_REG } from './../utils/regexp';
+
+export const useValidation = () => {
+  const [email, setEmail] = useState('');
+  const [emailValid, setEmailValid] = useState(false);
+  const [nickname, setNickName] = useState('');
+  const [nicknameValid, setNicknameValid] = useState(false);
+  const [pw, setPw] = useState('');
+  const [pwValid, setPwValid] = useState(false);
+  const [profile, setProfile] = useState<string>();
+
+  //   console.log(email);
+  //   console.log(pw);
+  //   console.log(profile);
+  //   console.log(nickname);
+  //   console.log(pwValid);
+  //   console.log(emailValid);
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setEmail(value);
+    const isValidEmail = value.match(EMAIL_REG);
+
+    isValidEmail ? setEmailValid(true) : setEmailValid(false);
+  };
+
+  const onChangePw = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    setPw(value);
+
+    const isValidPW = value.length < 5;
+
+    isValidPW ? setPwValid(false) : setPwValid(true);
+  };
+
+  const onChangeNickname = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setNickName(value);
+
+    const isValidNick = value.length < 1;
+
+    isValidNick ? setNicknameValid(false) : setNicknameValid(true);
+  };
+
+  const onChangeFile = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = (e.currentTarget.files as FileList)[0];
+
+    const url = URL.createObjectURL(file);
+
+    setProfile(url);
+  };
+
+  return {
+    onChangeEmail,
+    onChangePw,
+    onChangeFile,
+    onChangeNickname,
+    nicknameValid,
+    email,
+    nickname,
+    pw,
+    emailValid,
+    pwValid,
+    profile,
+  };
+};

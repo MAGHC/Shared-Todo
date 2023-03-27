144143b7460ea10bf62cd4889b5e08be2008d92b
MAGHC <windowsapple95@gmail.com>
2023-03-27 21:38:16 +0900

    feat: queryProvier, authProvider 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import LoginRegister from './pages/LoginRegister';
 import TodoPage from './pages/TodoPage';
+import AuthContextProvider from './context/AuthContextProvider';
 
 const router = createBrowserRouter([
   {
@@ -22,10 +28,16 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <RouterProvider router={router}></RouterProvider>
+      </AuthContextProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
 

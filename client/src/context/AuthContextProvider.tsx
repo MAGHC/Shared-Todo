154e25e4076ac144fb48dacdf23d86cf79a73bca
MAGHC <windowsapple95@gmail.com>
2023-03-27 21:38:44 +0900

    fix: 기존 auth 훅 삭제 authprovider로 병합

--- client/src/context/AuthContextProvider.tsx
+++ client/src/context/AuthContextProvider.tsx
@@ -1,14 +1,45 @@
 import { AuthContext } from './AuthContext';
+import { useFetch } from '../hooks/fetch';
+
+import { RegistBody, LoginBody, ResponseAuth } from '../type/auth';
+import axios, { AxiosError } from 'axios';
 
 import { useState, useEffect } from 'react';
-import { AuthContextT } from '../type/auth';
 
 const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
   const [email, setUserEmail] = useState<null | string>(null);
+  const { post } = useFetch();
 
-  useEffect(() => {}, []);
+  console.log(email, '여기는 컨텍스트');
 
-  return <AuthContext.Provider value={{ email }}>{children}</AuthContext.Provider>;
+  const login = async (body: LoginBody) => {
+    try {
+      const res = (await post('/auth/login', body)) as ResponseAuth;
+      setUserEmail(res.message);
+    } catch (err) {
+      const error = err as Error | AxiosError;
+
+      if (axios.isAxiosError(error)) {
+        console.log(error);
+      }
+    }
+  };
+
+  const regist = async (body: RegistBody) => {
+    try {
+      const res = (await post('/auth/regist', body)) as ResponseAuth;
+      setUserEmail(res.message);
+    } catch (err) {
+      const error = err as Error | AxiosError;
+      if (axios.isAxiosError(error)) {
+        console.log(error);
+      }
+    }
+  };
+
+  // useEffect(() => {}, []);
+
+  return <AuthContext.Provider value={{ email, login, regist }}>{children}</AuthContext.Provider>;
 };
 
 export default AuthContextProvider;

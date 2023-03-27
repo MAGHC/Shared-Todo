e1ab641b9d209ab5bd7ef003c13b9d1b3a831d3a
MAGHC <windowsapple95@gmail.com>
2023-03-27 22:14:29 +0900

    fix: 안쓰는 임포트삭제

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,9 +8,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import LoginRegister from './pages/LoginRegister';
-import TodoPage from './pages/TodoPage';
 import AuthContextProvider from './context/AuthContextProvider';
+import TodoPage from './pages/TodoPage';
 
 const router = createBrowserRouter([
   {

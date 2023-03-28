d1825295b76f9ece3708870e8673758d080aa9da
MAGHC <windowsapple95@gmail.com>
2023-03-28 21:44:27 +0900

    feat: EventBus Ui 및 기능 구현

--- client/src/context/AuthContextProvider.tsx
+++ client/src/context/AuthContextProvider.tsx
@@ -6,41 +6,70 @@ import axios, { AxiosError } from 'axios';
 
 import { useState } from 'react';
 import LoginRegister from './../pages/LoginRegister';
+import Notification from './../components/Common/Notification';
+import { EventBusI, useEventBus } from './EventBusContext';
+import TokenStorage from './../utils/token';
+
+const readAuth = () => {
+  const tokenstorage = new TokenStorage();
+
+  const token = tokenstorage.get();
+
+  return token ? JSON.stringify(token).toString() : null;
+};
 
 const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
-  const [email, setUserEmail] = useState<null | string>(null);
+  const [email, setUserEmail] = useState<null | string>(readAuth);
   const { post } = useFetch();
 
-  console.log(email, '여기는 컨텍스트');
+  const { show } = useEventBus() as EventBusI;
 
   const login = async (body: LoginBody) => {
+    show({ status: 'loading', message: '' });
     try {
       const res = (await post('/auth/login', body)) as ResponseAuth;
       setUserEmail(res.message);
+      show({ status: 'success', message: '성공적으로 로그인했습니다.' });
     } catch (err) {
       const error = err as Error | AxiosError;
 
       if (axios.isAxiosError(error)) {
-        console.log(error);
+        if (error.response) {
+          const errObj = error.response;
+
+          show({ status: 'error', message: `${errObj.status} 에러` });
+        }
       }
     }
   };
 
   const regist = async (body: RegistBody) => {
+    show({ status: 'loading', message: '' });
     try {
       const res = (await post('/auth/regist', body)) as ResponseAuth;
       setUserEmail(res.message);
+      show({ status: 'success', message: '성공적으로 회원가입했습니다.' });
     } catch (err) {
       const error = err as Error | AxiosError;
       if (axios.isAxiosError(error)) {
-        console.log(error);
+        if (error.response) {
+          const errObj = error.response;
+
+          show({ status: 'error', message: `${errObj.status} 에러` });
+        }
       }
     }
   };
 
   return (
     <AuthContext.Provider value={{ email, login, regist }}>
-      {email ? children : <LoginRegister></LoginRegister>}
+      {email ? (
+        <Notification>{children}</Notification>
+      ) : (
+        <Notification>
+          <LoginRegister></LoginRegister>
+        </Notification>
+      )}
     </AuthContext.Provider>
   );
 };

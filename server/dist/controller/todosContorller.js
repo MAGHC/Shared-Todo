ec75bf149665b8565f40ff193cc271cfa9ef3ef8
MAGHC <windowsapple95@gmail.com>
2023-03-31 17:04:38 +0900

    server: todomodel 모델

--- server/dist/controller/todosContorller.js
+++ server/dist/controller/todosContorller.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -10,27 +33,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.putTodo = exports.delTodo = exports.postTodo = exports.getByIdParam = exports.getTodos = void 0;
+const todoModel = __importStar(require("../model/todo"));
 const todos_1 = require("./../utils/todos");
-let todos = [
-    {
-        todoId: '1',
-        email: 'easdsa@naver.com',
-        createdAt: new Date(),
-        todo: '2021까지~다하기',
-        nickname: '시말',
-    },
-    {
-        todoId: '2',
-        email: 'easdsa@naver.com',
-        createdAt: new Date(),
-        todo: '2023까지~다하기',
-        nickname: '시2말',
-    },
-];
 function getTodos(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         const nickname = req.query.nickname;
-        const data = nickname ? todos.filter((todo) => todo.nickname === nickname) : todos;
+        const data = nickname
+            ? yield todoModel.getAllTodoByNick(nickname)
+            : yield todoModel.getAllTodos();
         res.status(200).json(data);
     });
 }
@@ -38,7 +48,7 @@ exports.getTodos = getTodos;
 function getByIdParam(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         const id = req.params.id;
-        const isExist = todos.find((todo) => todo.todoId === id);
+        const isExist = yield todoModel.getTodoById(id);
         if (isExist) {
             res.status(200).json(isExist);
         }
@@ -51,14 +61,7 @@ exports.getByIdParam = getByIdParam;
 function postTodo(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         const { email, todo, nickname } = req.body;
-        const newTodo = {
-            nickname,
-            todo,
-            email,
-            todoId: '3',
-            createdAt: new Date(),
-        };
-        todos = [newTodo, ...todos];
+        const newTodo = yield todoModel.createTodo(email, todo, nickname);
         res.status(201).json(newTodo);
     });
 }
@@ -66,9 +69,9 @@ exports.postTodo = postTodo;
 function delTodo(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         const id = req.params.id;
-        const isExist = todos.find((todo) => todo.todoId === id);
+        const isExist = yield todoModel.getTodoById(id);
         if (isExist) {
-            todos = todos.filter((todo) => todo.todoId !== id);
+            todoModel.deleteTodo(id);
             res.send(204);
         }
         else {
@@ -81,7 +84,7 @@ function putTodo(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         const { id } = req.params;
         const { todo } = req.body;
-        const isExist = todos.find((todo) => todo.todoId === id);
+        const isExist = yield todoModel.getTodoById(id);
         if (isExist) {
             isExist.todo = todo;
             res.status(200).json(isExist);

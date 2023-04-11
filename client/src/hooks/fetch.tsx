cedf06eab39452e6c709b15f06066aab56cd9c9f
MAGHC <windowsapple95@gmail.com>
2023-04-11 21:15:36 +0900

    fix :post, put ,delete header 추가

--- client/src/hooks/fetch.tsx
+++ client/src/hooks/fetch.tsx
@@ -1,6 +1,13 @@
 import { API } from '../utils/axios';
+import TokenStorage from '../utils/token';
+
+const token = new TokenStorage();
 
 export const useFetch = <T, V>() => {
+  const headers = {
+    Authorization: `Bearer ${token.get()}`,
+  };
+
   const get = async (url: string): Promise<T> => {
     const res = await API.get(url);
 
@@ -8,19 +15,19 @@ export const useFetch = <T, V>() => {
   };
 
   const post = async (url: string, body: V): Promise<T> => {
-    const res = await API.post(url, body);
+    const res = await API.post(url, body, { headers });
 
     return res.data;
   };
 
   const put = async (url: string, body: V): Promise<T> => {
-    const res = await API.put(url, body);
+    const res = await API.put(url, body, { headers });
 
     return res.data;
   };
 
   const del = async (url: string): Promise<T> => {
-    const res = await API.delete(url);
+    const res = await API.delete(url, { headers });
 
     return res.data;
   };
